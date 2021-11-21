6f29e670655531b44d9c22b8e4cf04e693490ab4
Faiz Siddiqui <faiz.siddiqui@coupa.com>
2021-11-21 23:27:53 +0530

    Initial commit

---
+++ lib/slacker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "slacker/version"
+
+module Slacker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/slacker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Slacker
+  VERSION = "0.1.0"
+end
